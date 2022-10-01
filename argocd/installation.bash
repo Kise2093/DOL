9b269186f398439a76e1564e3b475c4fa71ee9cc
Kise2093 <people12337@gmail.com>
2022-10-01 22:26:27 +0700

    what

--- argocd/installation.bash
+++ argocd/installation.bash
@@ -1,6 +1,10 @@
 kubectl create ns argo
 helm repo add argo https://argoproj.github.io/argo-helm
-helm -n argo install argo-cd argo/argo-cd --version 5.4.2
+helm -n argo upgrade --install argo-cd argo/argo-cd --version 5.4.1 \
+--set redis.enabled=false \
+--set redis-ha.persistentVolume.enabled=true \
+--set redis-ha.enabled=true
+
 kubectl label namespaces argo istio-injection=enabled
 
 kubectl -n argo create secret generic custom-ca --from-file=unique_name=/root/cert/custom-ca.pem
@@ -9,3 +13,9 @@ kubectl -n argo create secret generic custom-ca --from-file=unique_name=/root/ce
 kubectl -n argo apply -f gw.yaml
 
 kubectl -n argo apply -f vs.yaml
+
+
+k -n argo edit deployments.apps argo-cd-argocd-server
+
+add - --insecure 
+#in container env
